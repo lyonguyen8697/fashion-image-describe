87be66e4eeb2ba53267889d6d097e438e68dacc4
lyonguyen8697 <mr.locasdfg@gmail.com>
2019-05-11 22:24:35 +0700

    Init commit

---
+++ config.py
@@ -0,0 +1,69 @@
+
+class Config(object):
+    """ Wrapper class for various (hyper)parameters. """
+    def __init__(self):
+        # about the model architecture
+        self.cnn = 'resnet50'               # 'vgg16' or 'resnet50'
+        self.max_caption_length = 90
+        self.dim_embedding = 256
+        self.num_lstm_units = 512
+        self.num_initalize_layers = 2    # 1 or 2
+        self.dim_initalize_layer = 512
+        self.num_attend_layers = 2       # 1 or 2
+        self.dim_attend_layer = 512
+        self.num_decode_layers = 2       # 1 or 2
+        self.dim_decode_layer = 1024
+
+        # about the weight initialization and regularization
+        self.fc_kernel_initializer_scale = 0.08
+        self.fc_kernel_regularizer_scale = 1e-4
+        self.fc_activity_regularizer_scale = 0.0
+        self.conv_kernel_regularizer_scale = 1e-4
+        self.conv_activity_regularizer_scale = 0.0
+        self.fc_drop_rate = 0.5
+        self.lstm_drop_rate = 0.3
+        self.attention_loss_factor = 0.01
+
+        # about the optimization
+        self.num_epochs = 100
+        self.batch_size = 32
+        self.optimizer = 'Adam'    # 'Adam', 'RMSProp', 'Momentum' or 'SGD'
+        self.initial_learning_rate = 0.0001
+        self.learning_rate_decay_factor = 1.0
+        self.num_steps_per_decay = 100000
+        self.clip_gradients = 5.0
+        self.momentum = 0.0
+        self.use_nesterov = True
+        self.decay = 0.9
+        self.centered = True
+        self.beta1 = 0.9
+        self.beta2 = 0.999
+        self.epsilon = 1e-6
+
+        # about the saver
+        self.save_period = 7289
+        self.save_dir = './models/'
+        self.summary_dir = './summary/'
+
+        # about the vocabulary
+        self.vocabulary_file = './vocabulary.csv'
+        self.vocabulary_size = 1826
+        self.mean_file = './utils/ilsvrc_2012_mean.npy'
+
+        # about the training
+        self.train_image_dir = './dataset/images'
+        self.train_caption_file = './train/train-data.json'
+        self.temp_annotation_file = './train/anns.csv'
+        self.temp_data_file = './train/data.npy'
+
+        # about the evaluation
+        self.eval_image_dir = './dataset/images'
+        self.eval_caption_file = './val/val-data.json'
+        self.eval_result_dir = './val/results/'
+        self.eval_result_file = './val/results.json'
+        self.save_eval_result_as_image = True
+
+        # about the testing
+        self.test_image_dir = './test/images/'
+        self.test_result_dir = './test/results/'
+        self.test_result_file = './test/results.csv'
---
+++ utils/misc.py
@@ -0,0 +1,105 @@
+
+import numpy as np
+import cv2
+import heapq
+import Augmentor
+
+
+class ImageLoader(object):
+    def __init__(self, mean_file):
+        self.bgr = True
+        self.scale_shape = np.array([224, 224], np.int32)
+        self.crop_shape = np.array([224, 224], np.int32)
+        self.mean = np.load(mean_file).mean(1).mean(1)
+
+        self.maker = Augmentor.Pipeline()
+        self.maker.rotate(0.7, max_left_rotation=10, max_right_rotation=10)
+        self.maker.zoom(0.5, min_factor=1.1, max_factor=1.3)
+        self.maker.flip_left_right(0.5)
+        # self.maker.random_distortion(0.5, 5, 5, 5)
+        self.maker.skew(0.4, 0.5)
+
+    def image_distortion(self, image):
+        return self.maker._execute_with_array(image)
+
+    def load_image(self, image_file, distortion=False):
+        """ Load and preprocess an image. """
+        image = cv2.imread(image_file)
+
+        if distortion:
+            image = self.image_distortion(image)
+
+        if self.bgr:
+            temp = image.swapaxes(0, 2)
+            temp = temp[::-1]
+            image = temp.swapaxes(0, 2)
+
+        image = cv2.resize(image, (self.scale_shape[0], self.scale_shape[1]))
+        offset = (self.scale_shape - self.crop_shape) / 2
+        offset = offset.astype(np.int32)
+        image = image[offset[0]:offset[0]+self.crop_shape[0],
+                      offset[1]:offset[1]+self.crop_shape[1]]
+        # image = image - self.mean
+
+        return image
+
+    def load_images(self, image_files, distortion=False):
+        """ Load and preprocess a list of images. """
+        images = []
+        for image_file in image_files:
+            images.append(self.load_image(image_file, distortion))
+        images = np.array(images, np.float32)
+        return images
+
+
+class CaptionData(object):
+    def __init__(self, sentence, memory, output, score):
+       self.sentence = sentence
+       self.memory = memory
+       self.output = output
+       self.score = score
+
+    def __cmp__(self, other):
+        assert isinstance(other, CaptionData)
+        if self.score == other.score:
+            return 0
+        elif self.score < other.score:
+            return -1
+        else:
+            return 1
+
+    def __lt__(self, other):
+        assert isinstance(other, CaptionData)
+        return self.score < other.score
+
+    def __eq__(self, other):
+        assert isinstance(other, CaptionData)
+        return self.score == other.score
+
+
+class TopN(object):
+    def __init__(self, n):
+        self._n = n
+        self._data = []
+
+    def size(self):
+        assert self._data is not None
+        return len(self._data)
+
+    def push(self, x):
+        assert self._data is not None
+        if len(self._data) < self._n:
+            heapq.heappush(self._data, x)
+        else:
+            heapq.heappushpop(self._data, x)
+
+    def extract(self, sort=False):
+        assert self._data is not None
+        data = self._data
+        self._data = None
+        if sort:
+            data.sort(reverse=True)
+        return data
+
+    def reset(self):
+        self._data = []
